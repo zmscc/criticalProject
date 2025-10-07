76c4837bcac55020ecb051423b5d76d4a11694bd
zms <2631203133@qq.com>
2025-10-07 18:59:57 +0800

    Initial commit

---
+++ src/main/java/com/critical/HmDianPingApplication.java
@@ -0,0 +1,15 @@
+package com.critical;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.critical.mapper")
+@SpringBootApplication
+public class HmDianPingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmDianPingApplication.class, args);
+    }
+
+}
