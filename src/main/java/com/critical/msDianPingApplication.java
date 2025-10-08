f327096394ebfb5bed185fe17788382e4d2d6d48
zms <2631203133@qq.com>
2025-10-08 10:54:54 +0800

    功能实现

--- src/main/java/com/critical/msDianPingApplication.java
+++ src/main/java/com/critical/msDianPingApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.critical.mapper")
 @SpringBootApplication
-public class HmDianPingApplication {
+public class msDianPingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HmDianPingApplication.class, args);
+        SpringApplication.run(msDianPingApplication.class, args);
     }
 
 }
